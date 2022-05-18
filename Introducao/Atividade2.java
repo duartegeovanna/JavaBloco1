34a8cae8a8e6b6c5d726bfae04836d88384ed323
Geovanna Duarte <geovannaduarttes@gmail.com>
2022-05-18 18:15:45 -0300

    Introducao

--- Introducao/Atividade2.java
+++ Introducao/Atividade2.java
@@ -19,6 +19,36 @@ public class Atividade2 {
 		System.out.print("Digite o terceiro número: ");
 		n3=ler.nextInt();
 		
+		if (n1 >= n2 && n1 >= n3 && n2 >= n3) {
+
+			System.out.println(n3 + " - " + n2 + " - " + n1);
+			
+		} else if (n1 >= n2 && n1 >= n3 && n3 >= n2) {
+
+			System.out.println(n2 + " - " + n3 + " - " + n1);
+			
+		} else if (n2 >= n1 && n2 >= n3 && n1 >= n3) {
+
+			System.out.println(n3 + " - " + n1 + " - " + n2);
+			
+		} else if (n2 >= n1 && n2 >= n3 && n3 >= n1) {
+
+			System.out.println(n1 + " - " + n3 + " - " + n2);
+			
+		} else if (n3 >= n1 && n3 >= n2 && n2 >= n1) {
+
+			System.out.println(n1 + " - " + n2 + " - " + n3);
+			
+		} else if (n3 >= n1 && n3 >= n2 && n1 >= n2) {
+
+			System.out.println(n2 + " - " + n1 + " - " + n3);
+			
+		} else {
+			System.out.println("Entre com um valor válido.");
+		}
+		
+		ler.close();
+		
 	}
 
 }
